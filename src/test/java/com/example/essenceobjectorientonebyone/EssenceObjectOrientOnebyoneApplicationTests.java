3aaa9ba2c3e3062850c940bffe90c1a0ae92475d
aaronsleepy <aaron@kmong.com>
2022-07-15 09:57:13 +0900

    2022-7-15

---
+++ src/test/java/com/example/essenceobjectorientonebyone/EssenceObjectOrientOnebyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.essenceobjectorientonebyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssenceObjectOrientOnebyoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
